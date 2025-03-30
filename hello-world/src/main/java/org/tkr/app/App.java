1bff5e157d09cecb966b0e827da265fd33e9435a
Makoto Takezawa <makoto@takezawa.biz>
2025-03-30 12:13:03 +0900

    SonarQube の検証のため、ロジック追加

--- hello-world/src/main/java/org/tkr/app/App.java
+++ hello-world/src/main/java/org/tkr/app/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for ( int i = 0; i < 10 ; i++ ) {
+            System.out.println( "Number: " + i );
+            if ( i % 2 == 0 ) {
+                System.out.println( "Even number" );
+            } else {
+                System.out.println( "Odd number" );
+            }
+        }
     }
 }
